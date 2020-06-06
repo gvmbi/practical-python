bed7055ef10f075faa87c5d3aebb92767a3b09fa
Sergei Polianskii <sergepol@gmail.com>
2020-06-06 22:03:26 +0200

    Completed several excercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,18 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# ratio = 3 / 5 
+# height = 100.0
+# for x in range(10):
+#     height = height * ratio
+#     print(x + 1, round(height, 4))
+
+import urllib.request
+u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=14791&route=22')
+from xml.etree.ElementTree import parse
+doc = parse(u)
+for pt in doc.findall(
+    './/pt'
+):
+    print(pt.text)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,35 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+debth = 500000
+prc = 0.05
+payment = 2684.11
+total_paid = 0
+month = 0
+
+monthly_payment = 0
+
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while debth > 0:    
+    month += 1        
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:        
+        monthly_payment = payment + extra_payment               
+    else:        
+        monthly_payment = payment       
+
+    if debth - monthly_payment < 0:        
+        monthly_payment = debth        
+        debth = 0
+    else:
+        debth = debth * ( 1 + 0.05 / 12 ) - monthly_payment   
+
+    total_paid += monthly_payment
+    print(f'Month: {month:<5d} Payment: {monthly_payment:<10.2f} Debth: {debth:<10.2f}')
+
+print('--------')    
+print(f'{"Total paid:":<21s} {total_paid:<12.2f}')
+print(f'{"Number of month:":<21s} {month:<12d}')
